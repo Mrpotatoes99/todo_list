c9192372f9bb9bc5841b0064d74b96f78a7ac8a7
Mrpotates99 <23bgu0294_ms@psu.edu.ph>
2025-03-18 21:01:24 +0800

    Initial commit - To-Do List Project

---
+++ complete_task.php
@@ -0,0 +1,7 @@
+<?php
+require 'db.php';
+$id = $_GET['id'];
+$conn->query("UPDATE tasks SET is_completed=1 WHERE id=$id");
+header("Location: index.php");
+exit();
+?>
---
+++ delete_task.php
@@ -0,0 +1,7 @@
+<?php
+require 'db.php';
+$id = $_GET['id'];
+$conn->query("DELETE FROM tasks WHERE id=$id");
+header("Location: index.php");
+exit();
+?>
